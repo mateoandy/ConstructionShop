f2fabeaf81e789f180d3afcd6993f5a3ed65d0df
mateoandy <mateusz9614@onet.pl>
2023-04-14 11:57:01 +0200

    Construction shop

---
+++ src/main/java/com/project/ConstructionShop/ConstructionShopApplication.java
@@ -0,0 +1,13 @@
+package com.project.ConstructionShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructionShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstructionShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ConstructionShop/ConstructionShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ConstructionShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructionShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
